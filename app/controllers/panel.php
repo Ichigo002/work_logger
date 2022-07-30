0ae50e66cd6decb62f21d93e31a1a3dd7b8a4c7e
Ichigo002 <wiktorbojanowski28@wp.pl>
2022-07-30 22:07:42 +0200

    start making base of actions php

--- app/controllers/panel.php
+++ app/controllers/panel.php
@@ -3,13 +3,17 @@
 
     switch($type_action) {
         case 'btn_new_log': // new log
-
+            echo "New log";
+            // same thing like below
             break;
         case 'btn_up_rate': // Update a rate
-
+            echo "Update A Rate to: ".get("rate");
+            // same thing like below
             break;
         case 'btn_ch_pass': // change password
-
+            echo "Change Password to: ".get("pass1");
+            // make new small window with change password by JS and AJAX Jquery to send data to panel.php controller
+            // load window by JS from other file. NOT In the js [Optional]
             break;
         case 'btn_logout': // log out
             redirect(DEF_ADDRESS, array("pg_v" => "login/login", "stt" => "lgout"));
