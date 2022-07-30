257075d40093ca96978e3e07ca88671e97df18a9
Ichigo002 <wiktorbojanowski28@wp.pl>
2022-07-30 21:40:02 +0200

    make logout

--- app/controllers/panel.php
+++ app/controllers/panel.php
@@ -1,1 +1,19 @@
-<?php echo "Hello value: ". get("%typea%", "NOTHING!"); ?>
+<?php 
+    $type_action = get("%typea%", null);
+
+    switch($type_action) {
+        case 'btn_new_log': // new log
+
+            break;
+        case 'btn_up_rate': // Update a rate
+
+            break;
+        case 'btn_ch_pass': // change password
+
+            break;
+        case 'btn_logout': // log out
+            redirect(DEF_ADDRESS, array("pg_v" => "login/login", "stt" => "lgout"));
+            break;        
+    }
+?>
+
--- app/model/login/login.php
+++ app/model/login/login.php
@@ -1,11 +1,15 @@
 <?php
     $fail = false;
     $success = false;
+    $lg = false; // log out value
 
     if(get("stt") == "err301") {
         $fail = true;
     }
     if(get("stt") == "scc010") {
         $success = true;
+    }
+    if(get("stt") == "lgout") {
+        $lg = true;
     }
 ?>
