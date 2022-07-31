d02157046453ce39304c7a097d090c1537879676
Ichigo002 <wiktorbojanowski28@wp.pl>
2022-07-31 10:06:40 +0200

    start making controller

--- app/controllers/panel.php
+++ app/controllers/panel.php
@@ -3,19 +3,42 @@
 
     switch($type_action) {
         case 'btn_new_log': // new log
-            echo "New log";
-            // same thing like below
+            create_new_log();
             break;
         case 'btn_up_rate': // Update a rate
-            echo "Update A Rate to: ".get("rate");
-            // same thing like below
+            update_rate();
             break;
         case 'btn_ch_pass': // change password
-            echo "Change Password to: ".get("pass1");
+            change_password();
             break;
         case 'btn_logout': // log out
             redirect(DEF_ADDRESS, array("pg_v" => "login/login", "stt" => "lgout"));
             break;        
+    }
+
+    function create_new_log() {
+        $start_t = get("st_time", null);
+        $end_t = get("end_time", null);
+        $date = get("date", null);
+
+        echo $start_t.', '.$end_t.', '.$date;
+    }
+
+    function update_rate() {
+        $new_rate = get("rate", null);
+
+        echo $new_rate;
+    }
+
+    function change_password() {
+        $old_p = get("old-pass", null);
+        $new_p1 = get("pass1", null);
+        $new_p2 = get("pass2", null);
+
+        if($new_p1 != $new_p2) {
+            redirect(DEF_ADDRESS, array("pg_v" => "signed/panel"));
+            // KEEP GOING
+        }
     }
 ?>
 
